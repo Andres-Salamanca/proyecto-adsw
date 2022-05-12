f3ce1ea25cd6bc89de6a21b80582ecd41689da07
Andres-Salamanca <andrealebarbaritos@gmail.com>
2022-05-12 17:40:44 -0500

    subiendo archivos

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.parcialadsw {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.parcialadsw to javafx.fxml;
+    exports com.example.parcialadsw;
+}
