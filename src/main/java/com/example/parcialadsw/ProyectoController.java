d8a4585c81a940717feb694a676f05e6abbcce93
Kahsma <cajomapa@gmail.com>
2022-05-17 20:02:47 -0500

    Interfaz Y controladores basicos

--- src/main/java/com/example/parcialadsw/ProyectoController.java
+++ src/main/java/com/example/parcialadsw/ProyectoController.java
@@ -1,14 +1,68 @@
 package com.example.parcialadsw;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class ProyectoController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProyectoController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button bCargarArchivo;
+
+    @FXML
+    private ChoiceBox<String> bEscalafonoProfesor;
+    String [] valEscalafono ={"1","2","3","4"};
+
+    @FXML
+    private Button bGuardarArchivo;
+
+    @FXML
+    private Button botonCrearProfesor;
+
+    @FXML
+    private TextField campoCantSalMinProfesor;
+
+    @FXML
+    private TextField campoIDProfesor;
+
+    @FXML
+    private TextField campoNombreProfesor;
+
+    @FXML
+    private ComboBox<String> comboAsignaturasProfesor;
+    String [] asignaturas = {"POB","ADSw","BD","CYR","CDIO","CALCULO1","CALCULO2","ARQUITECTURA_PC"};
+
+    @FXML
+    private ListView<String> listaDependenciaProfesor;
+    String [] oficina = {"SUR","NORTE","CENTRAL","ORIENTE","OCCIDENTE", "H", "JB","NSDC"};
+
+    @FXML
+    void cargarArchivo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearProfesor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarArchivo(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        bEscalafonoProfesor.getItems().addAll(valEscalafono);
+        listaDependenciaProfesor.getItems().addAll(oficina);
+        comboAsignaturasProfesor.getItems().addAll(asignaturas);
     }
 }
